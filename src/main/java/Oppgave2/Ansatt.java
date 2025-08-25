de07ee7d3ed7e2226f504036ac55ee82a1ec0f6d
Patrik Thormodsen <patrik.mikal.thormodsen@gmail.com>
2025-08-25 13:49:44 +0200

    Oppgave 2 ferdiiig

---
+++ src/main/java/Oppgave2/Ansatt.java
@@ -0,0 +1,68 @@
+package Oppgave2;
+
+public class Ansatt {
+    private String fornavn;
+    private String etternavn;
+    private Kjonn kjonn;
+    private String stilling;
+    private int aarslonn;
+
+    public Ansatt(){
+
+    }
+
+    public Ansatt(String fornavn, String etternavn, Kjonn kjonn, String stilling, int aarslonn) {
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+        this.kjonn = kjonn;
+        this.stilling = stilling;
+        this.aarslonn = aarslonn;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+
+    public Kjonn getKjonn() {
+        return kjonn;
+    }
+
+    public String getStilling() {
+        return stilling;
+    }
+
+    public int getAarslonn() {
+        return aarslonn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public void setEtternavn(String etternavn) {
+        this.etternavn = etternavn;
+    }
+
+    public void setKjonn(Kjonn kjonn) {
+        this.kjonn = kjonn;
+    }
+
+    public void setStilling(String stilling) {
+        this.stilling = stilling;
+    }
+
+    public void setAarslonn(int aarslonn) {
+        this.aarslonn = aarslonn;
+    }
+
+    @Override
+    public String toString() {
+        return "Ansatt [fornavn=" + fornavn + ", etternavn=" + etternavn + ", kjonn=" + kjonn + ", stilling=" + stilling
+                + ", aarslonn=" + aarslonn + "]";
+    }
+
+}
---
+++ src/main/java/Oppgave2/Oppg2.java
@@ -0,0 +1,65 @@
+package Oppgave2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+
+public class Oppg2 {
+
+    public static void main(String[] args) {
+        List<Ansatt> ansatte = Arrays.asList(
+                new Ansatt("ansatt1", "etternavn1", Kjonn.MALE, "Stilling", 850000),
+                new Ansatt("ansatt2", "etternavn2", Kjonn.FEMALE, "Stilling", 950000),
+                new Ansatt("ansatt3", "etternavn3", Kjonn.MALE, "Stilling", 1050000),
+                new Ansatt("ansatt4", "etternavn4", Kjonn.FEMALE, "Stilling", 1150000),
+                new Ansatt("ansatt5", "etternavn5", Kjonn.MALE, "Stilling", 1250000)
+        );
+
+        //Fast kronetillegg
+        int kronetillegg = 50000;
+        lonnsoppgjor(ansatte, a -> a.getAarslonn() + kronetillegg);
+
+        //Fast prosenttillegg
+        double prosenttillegg = 1.05;
+        lonnsoppgjor(ansatte, a -> (int)(a.getAarslonn() * prosenttillegg));
+
+        //Fast kronetillegg vist lav lønn
+        int lavLonnKronetillegg = 20000;
+        int grenseLonn = 1000000;
+        lonnsoppgjor(ansatte, a -> {
+            if(a.getAarslonn() < grenseLonn){
+                return a.getAarslonn() + lavLonnKronetillegg;
+            } else {
+                return a.getAarslonn();
+            }
+        });
+
+        //Fast prosenttillegg vist mann
+        double mannTilegg = 1.5;
+        lonnsoppgjor(ansatte, a -> {
+            if(a.getKjonn() == Kjonn.MALE){
+                return (int)(a.getAarslonn() * mannTilegg);
+            } else {
+                return a.getAarslonn();
+            }
+        });
+
+        skrivUt(ansatte);
+
+    }
+
+    private static void skrivUt(List<Ansatt> ansatte){
+        for(Ansatt a : ansatte){
+            System.out.println(a.getFornavn() + " " + a.getEtternavn() + " " + a.getKjonn() + " " + a.getStilling() + " " + a.getAarslonn());
+        }
+    }
+
+    private static void lonnsoppgjor(List<Ansatt> ansatte, Function<Ansatt, Integer> funksjon){
+        for(Ansatt a : ansatte){
+            a.setAarslonn(funksjon.apply(a));
+        }
+    }
+
+}
