309d06da4541272c5adf1908d209a5031efa9d1a
Patrik Thormodsen <patrik.mikal.thormodsen@gmail.com>
2025-08-25 16:06:51 +0200

    Oppgave3

--- src/main/java/Oppgave3/Oppg3.java
+++ src/main/java/Oppgave3/Oppg3.java
@@ -4,6 +4,7 @@ import Oppgave2.Kjonn;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Oppg3 {
     public static void main(String[] args) {
@@ -11,9 +12,9 @@ public class Oppg3 {
         List<Ansatt> ansatte = Arrays.asList(
                 new Ansatt("ansatt1", "etternavn1", Kjonn.MALE, "Stilling", 850000),
                 new Ansatt("ansatt2", "etternavn2", Kjonn.FEMALE, "Stilling", 950000),
-                new Ansatt("ansatt3", "etternavn3", Kjonn.MALE, "Stilling", 1050000),
+                new Ansatt("ansatt3", "etternavn3", Kjonn.MALE, "Sjef", 1050000),
                 new Ansatt("ansatt4", "etternavn4", Kjonn.FEMALE, "Stilling", 1150000),
-                new Ansatt("ansatt5", "etternavn5", Kjonn.MALE, "Stilling", 1250000)
+                new Ansatt("ansatt5", "etternavn5", Kjonn.MALE, "Sjef", 1250000)
         );
 
         /*
@@ -32,8 +33,42 @@ public class Oppg3 {
         h) Finn ut summen av alle heltall i [1, 1000> som er delelig med 3 eller 5.
          */
 
+        //a
+        List<String> etternavn = ansatte.stream()
+                .map(Ansatt::getEtternavn)
+                .toList();
+        System.out.println(etternavn);
 
+        //b
+        long kvinner = ansatte.stream()
+                .filter(e -> (e.getKjonn() == Kjonn.FEMALE))
+                .count();
+        System.out.println(kvinner);
 
+        //c
+        double gjennomsnittKvinner = ansatte.stream()
+                .filter(e -> (e.getKjonn() == Kjonn.FEMALE))
+                .mapToInt(Ansatt::getAarslonn)
+                .average()
+                .orElse(0);
+        System.out.println(gjennomsnittKvinner);
 
+        //d
+        ansatte.stream()
+                .filter(e -> (e.getStilling().toLowerCase().contains("sjef")))
+                .forEach(e -> e.setAarslonn((int) (e.getAarslonn() * 1.07)));
+        ansatte.forEach(System.out::println);
+
+        //e
+        boolean erSant = ansatte.stream()
+                .anyMatch(e -> e.getAarslonn() > 800000);
+        System.out.println(erSant);
+
+        //f
+        ansatte.forEach(System.out::println);
+
+        //g
+        int minLonn = ansatte.stream()
+                .
     }
 }
